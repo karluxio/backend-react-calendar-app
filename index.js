d1d548a8f1126edd8ef68995ce636621ce97a671
karlux <karlux.sistemas@gmail.com>
2023-06-21 13:20:21 -0300

    feat: add public folder

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/api/v1', (req, res) => {
   return res.json({
     ok: true
   })
 })
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 3000
 
