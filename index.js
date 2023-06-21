bb9a32e8a96bc94ba12c3b2877a222845e63b9cf
karlux <karlux.sistemas@gmail.com>
2023-06-21 12:59:22 -0300

    feat: add basic server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('hola mundo!!!');
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({
+    ok: true
+  })
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+})
