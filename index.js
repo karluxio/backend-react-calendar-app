5f208daa3e86a94895bcb2b7b3e2940fd8bbf8a6
karlux <karlux.sistemas@gmail.com>
2023-06-22 01:42:46 -0300

    feat: add morgan for dev

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const path = require('path')
-const router = require('./routes/auth')
+const morgan = require('morgan')
 require('dotenv').config()
+
+const router = require('./routes/auth')
 
 const app = express()
 
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('dev'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', router)
