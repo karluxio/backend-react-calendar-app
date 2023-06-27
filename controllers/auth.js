5b8337f2c110c59f25239cc256fd2d85216557f6
karlux <karlux.sistemas@gmail.com>
2023-06-27 19:05:20 -0300

    feat: add save user in db without encrypting password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,40 @@
 const { response, request } = require('express')
+const User = require('../models/user')
 
-const registerController = (req = request, res = response) => {
+const registerController = async (req = request, res = response) => {
   const { name, email, password } = req.body
 
-  res.status(201).json({
-    ok: true,
-    msg: 'register',
-    name,
-    email,
-    password
-  })
+  let user = await User.findOne({ email })
+
+
+
+  if (user) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'email in use'
+    })
+  }
+
+  user = new User({ name, email, password })
+
+  try {
+
+    await user.save()
+    res.status(201).json({
+      ok: true,
+      uid: user.uid,
+      name: user.name
+    })
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'internal server error'
+    })
+  }
+
 }
 
 const loginController = (req, res = response) => {
