0f6939438b412be08faa1618a388e8c0c13e13b4
karlux <karlux.sistemas@gmail.com>
2023-06-28 14:16:58 -0300

    feat: add jwt to login and register

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,31 +2,36 @@ const { response, request } = require('express')
 const bcrypt = require('bcryptjs')
 
 const User = require('../models/user')
+const { generateJWT } = require('../helpers/jwt')
 
 const registerController = async (req = request, res = response) => {
   const { name, email, password } = req.body
 
-  let user = await User.findOne({ email })
-
-  if (user) {
-    return res.status(400).json({
-      ok: false,
-      msg: 'email in use'
-    })
-  }
-
-  user = new User({ name, email, password })
-
-  const salt = bcrypt.genSaltSync()
-  user.password = bcrypt.hashSync(password, salt)
-
   try {
+    let user = await User.findOne({ email })
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'email in use'
+      })
+    }
+
+    user = new User({ name, email, password })
+    const salt = bcrypt.genSaltSync()
+    user.password = bcrypt.hashSync(password, salt)
 
     await user.save()
+
+    // generate token
+    const token = await generateJWT(user._id, user.name)
+
     res.status(201).json({
       ok: true,
+      ok: true,
       uid: user._id,
-      name: user.name
+      name: user.name,
+      token
     })
 
   } catch (error) {
@@ -43,6 +48,7 @@ const registerController = async (req = request, res = response) => {
 const loginController = async (req, res = response) => {
   const { email, password } = req.body
 
+  // check if user exist
   try {
     const user = await User.findOne({ email })
     if (!user) {
@@ -52,6 +58,7 @@ const loginController = async (req, res = response) => {
       })
     }
 
+    // confirm password
     const validPassword = bcrypt.compareSync(password, user.password)
     console.log({ validPassword });
     if (!validPassword) {
@@ -61,9 +68,14 @@ const loginController = async (req, res = response) => {
       })
     }
 
+    // generate token
+    const token = await generateJWT(user._id, user.name)
+
     return res.json({
       ok: true,
-      msg: 'TODO: add a token'
+      uid: user._id,
+      name: user.name,
+      token
     })
 
   } catch (error) {
