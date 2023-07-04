2efc6dd2cd52bbbdb9ee5682d23a36b867e20c12
karlux <karlux.sistemas@gmail.com>
2023-07-04 17:54:43 -0300

    feat: update route of create event with its middlewares

--- routes/events.js
+++ routes/events.js
@@ -4,9 +4,12 @@
  */
 
 const { Router } = require('express')
+
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events')
 const fieldsValidator = require('../middlewares/fieldsValidator')
 const { validateJWT } = require('../middlewares/validate-jwt')
+const { check } = require('express-validator')
+const { isDate } = require('../helpers/isDate')
 
 const router = Router()
 
@@ -21,16 +24,28 @@ router.use(validateJWT)
 
 router.post(
   '/:id',
+  [
+    check('title', 'title is required').not().isEmpty(),
+    check('start', 'start date is required').custom(isDate),
+    check('end', 'end date is required').custom(isDate),
+    fieldsValidator
+  ],
   createEvent
 )
 
 router.put(
   '/:id',
+  [
+    fieldsValidator
+  ],
   updateEvent
 )
 
 router.delete(
   '/:id',
+  [
+    fieldsValidator
+  ],
   deleteEvent
 )
 
