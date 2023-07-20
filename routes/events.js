ccf43ec2ebc23a06360441f5c4877b09567d1d8b
karlux <karlux.sistemas@gmail.com>
2023-07-20 14:48:43 -0300

    fix: change route to create an event

--- routes/events.js
+++ routes/events.js
@@ -23,7 +23,7 @@ router.get(
 router.use(validateJWT)
 
 router.post(
-  '/:id',
+  '/',
   [
     check('title', 'title is required').not().isEmpty(),
     check('start', 'start date is required').custom(isDate),
